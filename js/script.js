befef84f0414a4addef58d6e44d6d22481f37d71
MGPasinetti <mg.pasinetti@gmail.com>
2022-03-15 16:55:42 +0100

    making scroll-chats section

---
+++ js/script.js
@@ -0,0 +1,55 @@
+const app = new Vue({
+    el: `#scroll-chats`,
+    data: {
+        arrAccounts: [
+            {
+                img: `img/avatar_1.jpg`,
+                name: `Michele`,
+                lastMsg: `Ultimo messaggio inviato`,
+                lastMsgTime: `12:00`,
+            },
+            {
+                img: `img/avatar_2.jpg`,
+                name: `Fabio`,
+                lastMsg: `Ultimo messaggio inviato`,
+                lastMsgTime: `12:00`,
+            },
+            {
+                img: `img/avatar_3.jpg`,
+                name: `Samuele`,
+                lastMsg: `Ultimo messaggio inviato`,
+                lastMsgTime: `12:00`,
+            },
+            {
+                img: `img/avatar_4.jpg`,
+                name: `Alessandro B.`,
+                lastMsg: `Ultimo messaggio inviato`,
+                lastMsgTime: `12:00`,
+            },
+            {
+                img: `img/avatar_5.jpg`,
+                name: `Alessandro L.`,
+                lastMsg: `Ultimo messaggio inviato`,
+                lastMsgTime: `12:00`,
+            },
+            {
+                img: `img/avatar_6.jpg`,
+                name: `Claudia`,
+                lastMsg: `Ultimo messaggio inviato`,
+                lastMsgTime: `12:00`,
+            },
+            {
+                img: `img/avatar_7.jpg`,
+                name: `Federico`,
+                lastMsg: `Ultimo messaggio inviato`,
+                lastMsgTime: `12:00`,
+            },
+            {
+                img: `img/avatar_8.jpg`,
+                name: `Davide`,
+                lastMsg: `Ultimo messaggio inviato`,
+                lastMsgTime: `12:00`,
+            },
+        ]
+    }
+});
