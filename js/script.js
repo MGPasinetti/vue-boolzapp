b32066752063efc4ee659dc153e24f2e03b47511
MGPasinetti <mg.pasinetti@gmail.com>
2022-03-16 11:45:52 +0100

    files fixed

--- js/script.js
+++ js/script.js
@@ -17,56 +17,56 @@ Pensate bene a come strutturare i dati prima di implementare il codice.
 
 
 const app = new Vue({
-    el: `#scroll-chats`,
+    el: `#app`,
     data: {
-        arrAccounts: [
+        arrContacts: [
             {
                 img: `img/avatar_1.jpg`,
                 name: `Michele`,
                 lastMsg: `Ultimo messaggio inviato`,
-                lastMsgTime: `12:00`,
+                lastMsgTime: `10/01/2020 16:15:22`,
             },
             {
                 img: `img/avatar_2.jpg`,
                 name: `Fabio`,
                 lastMsg: `Ultimo messaggio inviato`,
-                lastMsgTime: `12:00`,
+                lastMsgTime: `10/01/2020 16:15:22`,
             },
             {
                 img: `img/avatar_3.jpg`,
                 name: `Samuele`,
                 lastMsg: `Ultimo messaggio inviato`,
-                lastMsgTime: `12:00`,
+                lastMsgTime: `10/01/2020 16:15:22`,
             },
             {
                 img: `img/avatar_4.jpg`,
                 name: `Alessandro B.`,
                 lastMsg: `Ultimo messaggio inviato`,
-                lastMsgTime: `12:00`,
+                lastMsgTime: `10/01/2020 16:15:22`,
             },
             {
                 img: `img/avatar_5.jpg`,
                 name: `Alessandro L.`,
                 lastMsg: `Ultimo messaggio inviato`,
-                lastMsgTime: `12:00`,
+                lastMsgTime: `10/01/2020 16:15:22`,
             },
             {
                 img: `img/avatar_6.jpg`,
                 name: `Claudia`,
                 lastMsg: `Ultimo messaggio inviato`,
-                lastMsgTime: `12:00`,
+                lastMsgTime: `10/01/2020 16:15:22`,
             },
             {
                 img: `img/avatar_7.jpg`,
                 name: `Federico`,
                 lastMsg: `Ultimo messaggio inviato`,
-                lastMsgTime: `12:00`,
+                lastMsgTime: `10/01/2020 16:15:22`,
             },
             {
                 img: `img/avatar_8.jpg`,
                 name: `Davide`,
                 lastMsg: `Ultimo messaggio inviato`,
-                lastMsgTime: `12:00`,
+                lastMsgTime: `10/01/2020 16:15:22`,
             },
         ]
     }
